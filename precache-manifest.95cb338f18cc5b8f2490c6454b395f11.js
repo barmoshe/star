b6dd9d93990ef9298e1ca2faa64b8d692667b69c
barmoshe <1barmoshe1@gmail.com>
2024-06-06 02:09:14 +0300

    Updates

---
+++ precache-manifest.95cb338f18cc5b8f2490c6454b395f11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "713bb4c7215fd5f1d00ef8c3e8dd98ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5ae3a808bed1cd6033f",
+    "url": "/static/css/main.6e3d051b.chunk.css"
+  },
+  {
+    "revision": "4a443ac785a75b2d9326",
+    "url": "/static/js/2.109eb998.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.109eb998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47b61038b93d9dd7ad32",
+    "url": "/static/js/3.1500b281.chunk.js"
+  },
+  {
+    "revision": "e5ae3a808bed1cd6033f",
+    "url": "/static/js/main.8d936080.chunk.js"
+  },
+  {
+    "revision": "4aa7b273fc0f0c2d0419",
+    "url": "/static/js/runtime-main.fe4ded53.js"
+  }
+]);
